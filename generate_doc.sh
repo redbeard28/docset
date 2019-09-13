682fafd4ad75d26f8615912397a3b7953c186b63
Jeremie CUADRADO <jeremie_cuadrado@carrefour.com>
2019-09-13 08:41:22 +0200

    let's go for html2dash

--- generate_doc.sh
+++ generate_doc.sh
@@ -8,27 +8,23 @@ clear
 echo "
 ########################################
 #
-#   $(pwd)/$job_name -d /tmp/mydocs -D https://github.com/redbeard28/docset -i image_name -m serve
-#   $(pwd)/$job_name -d /tmp/mydocs -D . -i image_name -m html ('-D .' to use actual path)
+#   $(pwd)/$job_name -d /tmp/mydocs -i redbeard28/docset:1.0 -m html -V 0.0.3
 #
 #########################################
 option -d =>    /tmp/docs                                                   =>  path of your markdown
 option -i =>    redbeard28/docset:1.0                                       => Image Name
-option -D =>    https://github.com/redbeard28/redbeard-consulting_docs      => Dockerfile
 option -m =>    html or serve                                               => Generate html files or make server
+option -V =>    insert version number in the redbeard28.xml file
 "
 }
 
 #################################
 ### Recuperation des entrees
-while getopts d:D:i:m:P:h OPTION
+while getopts d:D:i:m:V:h OPTION
 do
   case "$OPTION" in
   d)
     DOCS_PATH="$OPTARG"
-    ;;
-  D)
-    DOCKERFILE="$OPTARG"
     ;;
   i)
     IMAGE_NAME="$OPTARG"
@@ -36,8 +32,8 @@ do
   m)
     TYPE="$OPTARG"
     ;;
-  P)
-    emplacement="$OPTARG"
+  V)
+    VERSION="$OPTARG"
     ;;
   h)
     usage
@@ -53,19 +49,6 @@ done
 
 generate_html() {
     echo "
-    ############################
-    #
-    # Generating $IMAGE_NAME
-    #
-    #
-    ############################
-
-    "
-    docker rm "$IMAGE_NAME"
-
-    cd "$DOCS_PATH"
-    docker build -t "$IMAGE_NAME" $DOCKERFILE
-    echo "
 
     ############################
     #
@@ -76,65 +59,25 @@ generate_html() {
 
     "
     cd "$DOCS_PATH"
-#    if [ -z "${emplacement}" ]; then
-#      rm -rf $emplacement
-#      path_docset=${emplacement}/redbeard28.docset
-#    else
-#        path='~/Library/Application\ Support/Dash/DocSets/Redbeard28'
-#        path_docset=${path}/redbeard28.docset
-#    fi
+
 
     docker run --rm -v "${DOCS_PATH}":/work "$IMAGE_NAME" build  &
     sleep 5
-    echo $emplacement
-    echo $path_docset
-    python html2dash.py -n redbeard28 -d ~/Library/Application\ Support/Dash/DocSets/Redbeard28 -i docs_src/images/icon.png docs
+
+    python html2dash.py -n Redbeard28 -d ${DOCS_PATH}/release -i docs_src/images/icon.png docs
     # Fix for icon.ico bug
-    cp docs_src/images/favicon.ico ~/Library/Application\ Support/Dash/DocSets/Redbeard28/redbeard28.docset/icon.ico
+    cp docs_src/images/favicon.ico release/Redbeard28.docset/icon.ico
+    cd ${DOCS_PATH}/release && tar --exclude='.DS_Store' -cvzf ${DOCS_PATH}/docs_src/feeds/redbeard28.tgz Redbeard28.docset
+
+    # Add new version
+    sed -i "s/TOTO/${VERSION}/g" ${DOCS_PATH}/docs_src/feeds/redbeard28.xml
 }
 
-generate_serve() {
-    echo "
-    ############################
-    #
-    # Generating $IMAGE_NAME
-    #
-    #
-    ############################
 
-    "
-    set -x
-    container_id="$(docker container ls|grep ${IMAGE_NAME}|awk '{ print $1 }')"
-    docker container stop  $container_id
-    #if [ $? -ne 0 ];then
-    #   echo "Command docker container stop $container_id faild"
-    #    exit 1
-    #fi
-
-    cd "$DOCS_PATH"
-    docker build -t $IMAGE_NAME $DOCKERFILE
-
-    echo "
-
-    ############################
-    #
-    # Create server at http://localhost:8003
-    #
-    #
-    ############################
-
-    "
-
-    cd "$DOCS_PATH"
-    docker run --rm -v "${DOCS_PATH}":/work -p 8003:8000 $IMAGE_NAME serve -a 0.0.0.0:8000 &
-}
 
 case "$TYPE" in
   html)
         generate_html
-        ;;
-  serve)
-        generate_serve
         ;;
   h)
     usage
